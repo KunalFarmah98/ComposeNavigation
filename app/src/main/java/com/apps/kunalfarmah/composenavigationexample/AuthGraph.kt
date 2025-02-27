b5363d8fbf9fb4bb3d76d8e28c8b060fee8121f4
Kunal Farmah <kunalfarmah98@gmail.com>
2025-02-28 00:59:55 +0530

    cleaned up popping logic

--- app/src/main/java/com/apps/kunalfarmah/composenavigationexample/AuthGraph.kt
+++ app/src/main/java/com/apps/kunalfarmah/composenavigationexample/AuthGraph.kt
@@ -3,25 +3,27 @@ package com.apps.kunalfarmah.composenavigationexample
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
+import androidx.navigation.navOptions
 import androidx.navigation.navigation
 
 fun NavGraphBuilder.AuthGraph(navController: NavHostController) {
     navigation<Screens.Auth>(startDestination = Screens.Login) {
+        // pop the Auth graph and start the new screen a fresh
+        val navOptions = navOptions {
+            popUpTo(Screens.Auth) { inclusive = true }
+            launchSingleTop = true
+        }
         composable<Screens.Login> {
             /**
              * Never pass navController to composables, instead expose a callback as an argument
              */
             LoginScreen(
                 onLogin = { response ->
-                    navController.navigate(Screens.Main(response.token, response.userId)) {
-                        popUpTo(Screens.Auth) { inclusive = true }
-                        launchSingleTop = true
-                    }
+                    navController.navigate(Screens.Main(response.token, response.userId), navOptions)
                 },
-                goToRegister = { navController.navigate(Screens.Register){
-                    popUpTo(Screens.Auth) { inclusive = true }
-                    launchSingleTop = true
-                } }
+                goToRegister = {
+                    navController.navigate(Screens.Register, navOptions)
+                }
             )
         }
 
@@ -31,15 +33,11 @@ fun NavGraphBuilder.AuthGraph(navController: NavHostController) {
              */
             RegisterScreen(
                 onRegister = { response ->
-                    navController.navigate(Screens.Main(response.token, response.userId)) {
-                        popUpTo(Screens.Auth) { inclusive = true }
-                        launchSingleTop = true
-                    }
+                    navController.navigate(Screens.Main(response.token, response.userId), navOptions)
                 },
-                goToLogin = { navController.navigate(Screens.Login){
-                    popUpTo(Screens.Auth) { inclusive = true }
-                    launchSingleTop = true
-                } }
+                goToLogin = {
+                    navController.navigate(Screens.Login, navOptions)
+                }
             )
         }
     }
